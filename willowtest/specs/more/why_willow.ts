928bb3d8e24e222f3a9850f11bf8387e301d76a3
Sam Gwilym <sam@gwil.garden>
2024-01-05 13:36:50 +0100

    Third draft!

--- willowtest/specs/more/why_willow.ts
+++ willowtest/specs/more/why_willow.ts
@@ -9,39 +9,29 @@ export const why_willow: Expression = site_template(
 				name: "why_willow",
 		},
 		[
-				pinformative(
-					"There are many kinds of ", sidenote("data network", "By ‘data network’ we mean the protocols, people, and physical infrastructure which help people share data with each other."), " in use today. What problems do existing networks cause, or fail to address?"
-				),
-				pinformative(
-					"The most problematic and ubiquitous networks today are those run by commercial entities. It’s abundantly clear that these commercial networks are both harmful ", em("and"), " unreliable."
-				),
-				pinformative(
-					"Their economic incentive has led to the invasive surveillance of a captive group of users who increasingly have labour and rent extracted from them."),
-					
-					pinformative("Naturally, there’s no money in ", em("protecting"), " users, which on a small scale has led to the unfettered harassment of individuals, and on a large scale led to social networks playing a “determining role” in ", sidenote("genocide", [
-						
-							[link("U.N. investigators cite Facebook role in Myanmar crisis", "https://www.reuters.com/article/us-myanmar-rohingya-facebook/u-n-investigators-cite-facebook-role-in-myanmar-crisis-idUSKCN1GO2PN/"), " Reuters, 12th March 2018. "]
-						,
-						[link("Meta in Myanmar", "https://erinkissane.com/meta-in-myanmar-part-i-the-setup"), " Erin Kissane, 28th September 2023."]
-					]), "."
-				),
-				pinformative(
-					"The dominant form of data network is also the most frail. These networks are housed in enormous data centres which require ", sidenote("exponentially", ["Yes, exponential. ", 
-						link("Energy Consumption of Datacenters", "https://media.ccc.de/v/37c3-11796-energy_consumption_of_datacenters"), " Thomas Fricke, 27th December 2023."
-					]), " increasing amounts of electricity and (often potable) water to operate. These facilities are brittle in the face of resource shortages, climate disaster, and are easily seized in times of political upheaval. They consume resources without moderation while an increasingly uncertain future comes into being around us."
-				),
-				pinformative(
-					"Removing commercial interest from a network drastically improves things. Federated networks (‘the fediverse’) offer a compelling alternative. But the architecture of federated servers puts pressure on (largely unpaid) volunteers to provide social and technical labour as server administrators. These always-online servers also rely on the same fragile infrastructure the commercial networks do."
-				),
-				pinformative(
-					"Further atomising the network brings us to peer-to-peer networking, where the burden of providing a network’s infrastructure is divided among its own users and their devices."
-				),
-				pinformative(
-					"Unfortunately, this arrangement is far more complicated than querying a single source of truth. In a bid to wrangle this complexity, many peer-to-peer protocol designers traded away basic features like editing and deletion, demurred on privacy, and employed data structures which are only capable of growing larger and larger. There is also an apparent preoccupation with global-scale networks to the exclusion of smaller, private ones."
-				),
-				pinformative(
-					"This is the problem space we want to reckon with."
-		), pinformative("We want Willow to be a credible solution to digital networking in an uncertain era. We want a protocol which can make running a network together a sustainable practice. We want a protocol resilient to inevitable change, and respectful of the resources we have."
-				),
+				pinformative("With so many existing protocols for data networks out there, why would we make Willow?"),
+				pinformative("We believe we’ve found a novel combination of techniques which allows Willow to satisfy many vital — and competing — criteria all at once."),
+				pinformative("We made Willow to make running a network ", em("together"), " a sustainable practice. A protocol which enables a plurality of small, private networks alongside big, public ones. A protocol where the burden of running infrastructure is divided among its users, with no volunteer server admins."),
+				pinformative("We made Willow to be a credible solution to digital networking in an uncertain era. It must be resilient in the times we’re ", em("forced"), " to scale down, whether that’s due to a temporary loss of signal, natural disaster, or war. It must be respectful of the resources we’re left with, and able to run on low-spec hardward and on low-bandwidth networks."),
+				pinformative("We made Willow to be private, so that it’s possible to find people with common interests without broadcasting those interests to the world, and so that it’s possible to let others distribute data on your behalf without letting them know what that data ", em("is"), "."),
+				pinformative("We made Willow to reconcile peer-to-peer networks with social realities. Wrangling the complexity of distributed systems shouldn’t mean we trade away basic features like deletion, or accept data structures which can only grow without limit."),
+				pinformative("We made Willow because we wanted to do something to our best of our ability, and in the light of the kind of world we’d like to see.")
 		],
 );
+
+/*
+
+
+
+
+
+	
+
+	
+
+
+
+	
+
+
+*/
