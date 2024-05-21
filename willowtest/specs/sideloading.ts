6796e059eaae8de68f53e81b177f9da63dd21d90
Sam Gwilym <sam@gwil.garden>
2024-05-21 12:11:30 +0100

    Finish sideloading protocol section

--- willowtest/specs/sideloading.ts
+++ willowtest/specs/sideloading.ts
@@ -2,9 +2,10 @@ import { Expression } from "macro";
 import { pinformative, site_template, lis, link, def_parameter_fn, def_value } from "../main.ts";
 import { def, preview_scope, r, rs } from "../../defref.ts";
 import { link_name } from "../../linkname.ts";
-import { em } from "../../h.ts";
+import { code, em } from "../../h.ts";
 import { hsection } from "../../hsection.ts";
 import { marginale } from "../../marginalia.ts";
+import { field_access, function_call } from "../../pseudocode.ts";
 
 export const sideloading: Expression = site_template(
 	{
@@ -45,12 +46,45 @@ export const sideloading: Expression = site_template(
 				["An ", r("encoding_function"), " for ", r("SubspaceId")],
 				["An ", r("encoding_function"), " for ", r("PayloadDigest")],
 				["An ", r("encoding_function"), " for ", r("AuthorisationToken")],
+				["A ", r("NamespaceId"), " ", def_value({id:"side_default_namespace_id", singular: "default_namespace_id" })],
+				["A ", r("SubspaceId"), " ", def_value({id:"side_default_subspace_id", singular: "default_subspace_id" })],
+				["A ", r("PayloadDigest"), " ", def_value({id:"side_default_payload_digest", singular: "default_payload_digest" })],
 				["And a function which encrypts the final compiled encoding, ", def_parameter_fn({id: "encrypt"}), "."]
 			)
 		]),
 		
 		hsection("sideload_protocol", "Protocol", [
-		
+			pinformative("Let ", def_value({id: "side_entries", singular: 'entries'}), " be an arbitrarily selected set of ", r("PossiblyAuthorisedEntry"), " from the ", r("namespace"), " ", def_value({id: "namespace_n", singular: 'namespace'}), " in a specific order."),
+			pinformative(
+				marginale(["This ordering is used to take maximum advantage of the relative encodings used."]),
+				"An ", r("PossiblyAuthorisedEntry"), "'s entry ", def_value({id: "side_ord_e1", singular: "e1"}), " is prior to an ", r("PossiblyAuthorisedEntry"), "'s entry ", def_value({id: "side_ord_e2", singular: "e2"}), " if ", lis(
+					[
+						code(field_access(r("side_ord_e2"), "entry_subspace_id"), " < ", field_access(r("side_ord_e1"), "entry_subspace_id")), ", or",
+					],
+					[
+						code(field_access(r("side_ord_e2"), "entry_path"), " < ", field_access(r("side_ord_e1"), "entry_path")), ", or",
+					],
+					[
+						code(field_access(r("side_ord_e2"), "entry_timestamp"), " < ", field_access(r("side_ord_e1"), "entry_timestamp")), "."
+					],
+				)),
+				pinformative([
+					"Let ", def_value("initial_entry"), " be the result of ", code(function_call(r("default_entry"), r("side_default_namespace_id"), r("side_default_subspace_id"), r("side_default_payload_digest")), ".")
+				]),
+				pinformative("Let ", def_value({id: "side_contents", singular: "contents"}), " be the concatenation of", lis(
+					["An unsigned 64 bit integer of the length of ", r("side_entries"), " followed by",],
+					[	"Each ", r("PossiblyAuthorisedEntry"), " of ",  r("side_entries"), " mapped to triples of ", code("[", def_value({id: "side_encoded_entry", singular: "encoded_entry"}), ", ", def_value({id: "side_encoded_token", singular: "encoded_token"}), ", ", def_value({id: "side_payload", singular: "payload"}), "]"), " where ", def_value({id: "side_entry", singular: "entry"}), " and ", def_value({id: "side_token", singular: 'token'}), " are the respective elements of ", r("AuthorisationToken"), ", and", lis(
+						[
+							r("side_encoded_entry"), " is the result of ", code(function_call(r("encode_entry_relative_entry"), r("side_entry"), r('side_prior_entry'), )), " where ", marginale(["And if ", r("side_entry"), " is the first entry, ", r("side_prior_entry"), " is ", r("initial_entry"), '.']), def_value({id: 'side_prior_entry', singular: "prior_entry"}), " is the ", r("Entry"), " prior to this one in ", r("side_entries"), ".",
+						],
+						[ " where ", r("side_encoded_token"), " is the result of encoding ", r("side_token") ],
+						["and ", r("side_payload"), " is the complete ", r("Payload"), " of ", r("side_entry"), "." ] 
+					)],
+				), 
+				pinformative("Finally, let ", def_value({id: "side_drop", singular: "drop"}), " be the result of", code(function_call(r("encrypt"), r("side_contents"))), ".")),
+				hsection("sideload_transport", "Transport", [
+					"Once created, ", r("side_drop"), " can be transported by whatever means a single bytestring can be transferred, to be decrypted and ", r("side_contents"), " ingested by its intended recipient."  
+				])
 		])
 	],
 	
