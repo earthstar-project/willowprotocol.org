22f61b870569a7d806093fde46c26e5ef09730cd
Aljoscha Meyer <mail@aljoscha-meyer.de>
2025-05-12 10:36:54 +0200

    Port projects page

--- src/pages/more/projects_and_communities.tsx
+++ src/pages/more/projects_and_communities.tsx
@@ -1,21 +1,9 @@
 import { Dir, File } from "macromania-outfs";
-import { AE, Alj, Curly, NoWrap, Path } from "../../macros.tsx";
+import { AE } from "../../macros.tsx";
 import { PageTemplate } from "../../pageTemplate.tsx";
-import { Code, Img, Li, P, Ul } from "macromania-html";
-import { ResolveAsset } from "macromania-assets";
-import { Marginale, Sidenote } from "macromania-marginalia";
+import { Li, P, Ul } from "macromania-html";
 import { Hsection } from "macromania-hsection";
-import { Def, R, Rb, Rs } from "macromania-defref";
-import {
-  AccessStruct,
-  DefFunction,
-  DefType,
-  DefValue,
-  StructDef,
-} from "macromania-rustic";
-import { M } from "macromania-katex";
-import { PreviewScope } from "macromania-previews";
-import { Pseudocode } from "macromania-pseudocode";
+import { R } from "macromania-defref";
 
 export const projects_and_communities = (
   <Dir name="projects_and_communities">
@@ -23,52 +11,140 @@ export const projects_and_communities = (
       <PageTemplate
         htmlTitle="Active Projects and Communities"
         headingId="projects_and_communities"
-        heading={"Active Projects and Communities"}
+        heading="Active Projects and Communities"
       >
         <P>
-          <Alj inline>TODO</Alj>
+          Below is an overview of active projects related to Willow, as well as
+          some links to places where you can connect with other people
+          interested in the project.
         </P>
 
-        {/* pinformative("Below is an overview of active projects related to Willow, as well as some links to places where you can connect with other people interested in the project."),
-			hsection('willow_implementations', "Implementations", [
-				pinformative("There are several funded, open source implementations of Willow being worked on."),
-				pinformative("If you would like to implement Willow in your favourite language, we’ll gladly answer any questions you may have! Please see the ", link_name("community_and_contact", "community and contact"), " section below for some ways of getting in touch."),
-				
-				hsection("earthstar_project_implementations", "Earthstar Project", [
-					pinformative("Willow is brought to you by the ", link("Earthstar Project", "https://earthstar-project.org"), ". We aim to provide implementations of Willow specifications in both TypeScript and Rust."),
-					pinformative("We have complete TypeScript implementations of Meadowcap and Willow (comprising the ", link_name("data_model", "Willow Data Model"), ", ", link_name("sync", "Willow General Purpose Sync protocol"), ", and ", link_name("sideloading", "Willow Sideloading protocol"), "). These implementations have been funded by ", link("NGI Assure", "https://nlnet.nl/assure/"), ":"),
-					lis(
-						link("meadowcap-js", "https://github.com/earthstar-project/meadowcap-js"),
-						link("willow-js", "https://github.com/earthstar-project/willow-js"),		
-					),
-					
-					pinformative("We are currently working on ", link("Rust implementations of the Willow specifications", "https://github.com/earthstar-project/willow-rs"), ", with our work funded by ", link("NGI Core", "https://nlnet.nl/core/"), ". As the time of writing, we have released the following crates:"),
-					lis(
-						link("willow-data-model", "https://docs.rs/willow-data-model/0.1.0/willow_data_model/"),
-						link("meadowcap", "https://docs.rs/meadowcap/0.1.0/meadowcap/")
-					),
-				]),
-				
-				hsection("iroh_implementations", "Iroh", [
-					pinformative("Iroh is an open-source framework for p2p applications adopting the ", link_name("data_model", "Willow Data Model"), ", ", link_name("meadowcap", "Meadowcap"), ", and the ", link_name("sync", "Willow General Purpose Sync protocol"), ". They are users and contributors to our Rust implementations."),
-					lis(
-						link("Iroh website", "https://iroh.computer"),
-						link("Iroh repo", "https://github.com/n0-computer/iroh")
-					)
-				])
-			]),
-			hsection("community_and_contact", "Community and Contact", [
-				lis(
-					link("Email us",  "mailto:mail@aljoscha-meyer.de,sam@gwil.garden"),
-					link("Earthstar Project Open Collective", "https://opencollective.com/earthstar"),
-					link("Earthstar Project Discord", "https://discord.gg/6NtYzQC2G4"),
-					link("gwil on Mastodon", "https://post.lurk.org/@gwil"),
-				)
-			]),
-			hsection("this_website", "This site", [
-				pinformative("This website is also an active project. The repository for it can be found ", link("here", "https://github.com/earthstar-project/willowprotocol.org"), ". We appreciate all feedback from typos, to stumbling blocks, or any errant American English spellings you find.")
-			]) */}
+        <Hsection n="willow_implementations" title="Implementations">
+          <P>
+            There are several funded, open source implementations of Willow
+            being worked on.
+          </P>
 
+          <P>
+            If you would like to implement Willow in your favourite language,
+            we’ll gladly answer any questions you may have! Please see the{" "}
+            <R n="community_and_contact">community and contact</R>{" "}
+            section below for some ways of getting in touch.
+          </P>
+
+          <Hsection
+            n="earthstar_project_implementations"
+            title="Earthstar Project"
+          >
+            <P>
+              Willow is brought to you by the{" "}
+              <AE href="https://earthstar-project.org">Earthstar Project</AE>.
+              We aim to provide implementations of Willow specifications in both
+              TypeScript and Rust.
+            </P>
+          </Hsection>
+
+          <P>
+            We are currently working on{" "}
+            <AE href="https://github.com/earthstar-project/willow-rs">
+              Rust implementations of the Willow specifications
+            </AE>, with our work funded by{" "}
+            <AE href="https://nlnet.nl/core/">NGI Core</AE>. At the time of
+            writing, we have released the following crates:
+          </P>
+
+          <Ul>
+            <Li>
+              <AE href="https://docs.rs/willow-data-model/0.1.0/willow_data_model/">
+                willow-data-model
+              </AE>
+            </Li>
+            <Li>
+              <AE href="https://docs.rs/meadowcap/0.1.0/meadowcap/">
+                meadowcap
+              </AE>
+            </Li>
+          </Ul>
+
+          <P>
+            We have TypeScript implementations of older versions of Meadowcap
+            and Willow (comprising the <R n="data_model">Willow Data Model</R>,
+            {" "}
+            <R n="sync">Willow General Purpose Sync protocol</R>, and{" "}
+            <R n="sideloading">Willow Sideloading protocol</R>). These
+            implementations have been funded by{" "}
+            <AE href="https://nlnet.nl/assure/">NGI Assure</AE>, and we hope to
+            be able to update them to the final state of the specifications
+            soon:
+          </P>
+
+          <Ul>
+            <Li>
+              <AE href="https://github.com/earthstar-project/meadowcap-js">
+                meadowcap-js
+              </AE>
+            </Li>
+            <Li>
+              <AE href="https://github.com/earthstar-project/willow-js">
+                willow-js
+              </AE>
+            </Li>
+          </Ul>
+        </Hsection>
+
+        <Hsection n="iroh_implementations" title="Iroh">
+          <P>
+            Iroh is an open-source framework for p2p applications adopting the
+            {" "}
+            <R n="data_model">Willow Data Model</R>,{" "}
+            <R n="meadowcap">Meadowcap</R>, and the{" "}
+            <R n="sync">Willow General Purpose Sync protocol</R>. They are users
+            and contributors to our Rust implementations.
+          </P>
+
+          <Ul>
+            <Li>
+              <AE href="https://iroh.computer">Iroh website</AE>
+            </Li>
+            <Li>
+              <AE href="https://github.com/n0-computer/iroh">Iroh repo</AE>
+            </Li>
+          </Ul>
+        </Hsection>
+
+        <Hsection n="community_and_contact" title="Community and Contact">
+          <Ul>
+            <Li>
+              <AE href="mailto:mail@aljoscha-meyer.de,sam@gwil.garden">
+                Email us
+              </AE>
+            </Li>
+            <Li>
+              <AE href="https://opencollective.com/earthstar">
+                Earthstar Project Open Collective
+              </AE>
+            </Li>
+            <Li>
+              <AE href="https://discord.gg/6NtYzQC2G4">
+                Earthstar Project Discord
+              </AE>
+            </Li>
+            <Li>
+              <AE href="https://post.lurk.org/@gwil">gwil on Mastodon</AE>
+            </Li>
+          </Ul>
+        </Hsection>
+
+        <Hsection n="this_website" title="This Site">
+          <P>
+            This website is also an active project. The repository for it can be
+            found{" "}
+            <AE href="https://github.com/earthstar-project/willowprotocol.org">
+              here
+            </AE>. We appreciate all feedback from typos, to stumbling blocks,
+            or any errant American English spellings you find.
+          </P>
+        </Hsection>
       </PageTemplate>
     </File>
   </Dir>
