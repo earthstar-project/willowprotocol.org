8c9c558ccfb5becfc3803f8cce6c6527339e28a8
Aljoscha Meyer <mail@aljoscha-meyer.de>
2025-05-20 09:20:47 +0200

    Add etag generation

--- src/pageTemplate.tsx
+++ src/pageTemplate.tsx
@@ -34,6 +34,7 @@ import { Hsection, TableOfContents } from "macromania-hsection";
 import { R } from "macromania-defref";
 import { ResolveAsset } from "macromania-assets";
 import { CssDependency, JsDependency } from "macromania-previews";
+import { encodeHex } from "jsr:@std/encoding/hex";
 
 export function Page(
   props: PageTemplateProps & { children?: Expressions; name: string },
@@ -98,173 +99,205 @@ export function PageTemplate(
         forceLang // Ensure the locale you specified is used even if the citation style specifies a default locale.
         items={bib}
       >
-        <Html5
-          htmlProps={{ lang: "en-GB" }}
-          bodyProps={{
-            clazz: `${status === "proposal" ? "proposal" : "bg"}`,
+        <map
+          fun={async (rendered, _ctx) => {
+            const pageBuffer = new TextEncoder().encode(rendered);
+            const hashBuffer = await crypto.subtle.digest(
+              "SHA-256",
+              pageBuffer,
+            );
+            const hash = encodeHex(hashBuffer);
+
+            return (
+              <>
+                <File name="etag">
+                  {hash}
+                </File>
+                {rendered}
+              </>
+            );
           }}
-          title={<>Willow Specifications - {<exps x={htmlTitle} />}</>}
-          headContents={
-            <>
-              <Meta
-                name="viewport"
-                content="width=device-width, initial-scale=1"
-              />
-              <Meta
-                name="description"
-                content={htmlDescription
-                  ? <exps x={htmlDescription} />
-                  : "Protocols for synchronisable data stores. The best parts? Fine-grained permissions, a keen approach to privacy, destructive edits, and a dainty bandwidth and memory footprint."}
-              />
-              <Script>let FF_FOUC_FIX;</Script>
-              {/*to prevent Firefox FOUC, this must be here*/}
-              <Link
-                rel="alternate"
-                type="application/rss+xml"
-                href="/rss_news.xml"
-                title="Willow News and Updates"
-              />
-              <Link
-                rel="alternate"
-                type="application/rss+xml"
-                href="/rss_changelog.xml"
-                title="Willow Specification Changelog"
-              />
-              <Link
-                rel="icon"
-                href="/assets/favicon.svg"
-                type="image/svg+xml"
-              />
-              <Link rel="icon" href="/assets/favicon.png" type="image/png" />
-              <Link
-                rel="apple-touch-icon"
-                href="/assets/apple-touch-icon.png"
-              />
-            </>
-          }
         >
-          <CssDependency dep={["index.css"]} />
-          {toc
-            ? (
-              <JsDependency
-                dep={["toc.js"]}
-                scriptProps={{ defer: true }}
-              />
-            )
-            : ""}
+          <Html5
+            htmlProps={{ lang: "en-GB" }}
+            bodyProps={{
+              clazz: `${status === "proposal" ? "proposal" : "bg"}`,
+            }}
+            title={<>Willow Specifications - {<exps x={htmlTitle} />}</>}
+            headContents={
+              <>
+                <Meta
+                  name="viewport"
+                  content="width=device-width, initial-scale=1"
+                />
+                <Meta
+                  name="description"
+                  content={htmlDescription
+                    ? <exps x={htmlDescription} />
+                    : "Protocols for synchronisable data stores. The best parts? Fine-grained permissions, a keen approach to privacy, destructive edits, and a dainty bandwidth and memory footprint."}
+                />
+                <Script>let FF_FOUC_FIX;</Script>
+                {/*to prevent Firefox FOUC, this must be here*/}
+                <Link
+                  rel="alternate"
+                  type="application/rss+xml"
+                  href="/rss_news.xml"
+                  title="Willow News and Updates"
+                />
+                <Link
+                  rel="alternate"
+                  type="application/rss+xml"
+                  href="/rss_changelog.xml"
+                  title="Willow Specification Changelog"
+                />
+                <Link
+                  rel="icon"
+                  href="/assets/favicon.svg"
+                  type="image/svg+xml"
+                />
+                <Link rel="icon" href="/assets/favicon.png" type="image/png" />
+                <Link
+                  rel="apple-touch-icon"
+                  href="/assets/apple-touch-icon.png"
+                />
+              </>
+            }
+          >
+            <CssDependency dep={["index.css"]} />
+            {toc
+              ? (
+                <JsDependency
+                  dep={["toc.js"]}
+                  scriptProps={{ defer: true }}
+                />
+              )
+              : ""}
 
-          <Div id="wrapContent">
-            <Main>
-              <Hsection title={<exps x={heading} />} n={headingId}>
-                {toc ? <TableOfContents headerExprs={
-                  <Nav>
-                    <Li>⇡ <R n={parentId || "willow"}/></Li>
-                    <Li><R n={headingId}/></Li>
-                  </Nav>} stopLevel={99} /> : ""}
-                {status
-                  ? (
-                    <P>
-                      <RenderSpecStatus status={status} /> {statusDate
-                        ? (
-                          <>
-                            (as of <exps x={statusDate} />)
-                          </>
-                        )
-                        : ""}
-                    </P>
-                  )
-                  : ""}
-                <exps x={children} />
-                {bibliography
-                  ? (
-                    <Hsection
-                      title="References"
-                      n={`${headingId}_bibliography`}
-                      noNumbering
-                    >
-                      <Bibliography />
-                    </Hsection>
-                  )
-                  : ""}
-              </Hsection>
-            </Main>
+            <Div id="wrapContent">
+              <Main>
+                <Hsection title={<exps x={heading} />} n={headingId}>
+                  {toc
+                    ? (
+                      <TableOfContents
+                        headerExprs={
+                          <Nav>
+                            <Li>
+                              ⇡ <R n={parentId || "willow"} />
+                            </Li>
+                            <Li>
+                              <R n={headingId} />
+                            </Li>
+                          </Nav>
+                        }
+                        stopLevel={99}
+                      />
+                    )
+                    : ""}
+                  {status
+                    ? (
+                      <P>
+                        <RenderSpecStatus status={status} /> {statusDate
+                          ? (
+                            <>
+                              (as of <exps x={statusDate} />)
+                            </>
+                          )
+                          : ""}
+                      </P>
+                    )
+                    : ""}
+                  <exps x={children} />
+                  {bibliography
+                    ? (
+                      <Hsection
+                        title="References"
+                        n={`${headingId}_bibliography`}
+                        noNumbering
+                      >
+                        <Bibliography />
+                      </Hsection>
+                    )
+                    : ""}
+                </Hsection>
+              </Main>
 
-            <Footer>
-              <Nav>
-                <Ul>
-                  <Li>
-                    <R n="willow">Home</R>
-                  </Li>
-                  <Li>
-                    <R n="specifications">Specs</R>
-                  </Li>
-                  <Li>
-                    <R n="changes">News</R>
-                  </Li>
-                  <Li>
-                    <R n="about">About Us</R>
-                  </Li>
-                  <Li>
-                    <R n="projects_and_communities">Active Projects</R>
-                  </Li>
-                  <Li>
-                    <R n="more">More</R>
-                  </Li>
-                </Ul>
-              </Nav>
+              <Footer>
+                <Nav>
+                  <Ul>
+                    <Li>
+                      <R n="willow">Home</R>
+                    </Li>
+                    <Li>
+                      <R n="specifications">Specs</R>
+                    </Li>
+                    <Li>
+                      <R n="changes">News</R>
+                    </Li>
+                    <Li>
+                      <R n="about">About Us</R>
+                    </Li>
+                    <Li>
+                      <R n="projects_and_communities">Active Projects</R>
+                    </Li>
+                    <Li>
+                      <R n="more">More</R>
+                    </Li>
+                  </Ul>
+                </Nav>
 
-              <Div>
-                <Marginale inlineable>
-                  <A href="https://nlnet.nl" clazz="funder">
-                    <Img
-                      src={<ResolveAsset asset={["graphics", "nlnet.svg"]} />}
-                      alt="The logo of the NLnet foundation."
-                    />
-                  </A>
-                </Marginale>
-                <P>
-                  This project was funded through the NGI Assure Fund, a fund
-                  established by NLnet with financial support from the European
-                  Commission’s Next Generation Internet programme, under the
-                  aegis of DG Communications Networks, Content and Technology
-                  under grant agreement № 957073.
-                </P>
+                <Div>
+                  <Marginale inlineable>
+                    <A href="https://nlnet.nl" clazz="funder">
+                      <Img
+                        src={<ResolveAsset asset={["graphics", "nlnet.svg"]} />}
+                        alt="The logo of the NLnet foundation."
+                      />
+                    </A>
+                  </Marginale>
+                  <P>
+                    This project was funded through the NGI Assure Fund, a fund
+                    established by NLnet with financial support from the
+                    European Commission’s Next Generation Internet programme,
+                    under the aegis of DG Communications Networks, Content and
+                    Technology under grant agreement № 957073.
+                  </P>
 
-                <Div clazz="clearRight" />
+                  <Div clazz="clearRight" />
 
-                <Marginale inlineable>
-                  <A href="https://www.tu.berlin/" clazz="funder">
-                    <Img
-                      src={
-                        <ResolveAsset asset={["graphics", "tu_berlin.svg"]} />
-                      }
-                      alt="The logo of the TU Berlin."
-                    />
-                  </A>
-                </Marginale>
-                <P>
-                  <R n="aljoscha" /> has been employed at{" "}
-                  <AE href="https://www.tu.berlin/">TU Berlin</AE>{" "}
-                  while working on Willow.
-                </P>
+                  <Marginale inlineable>
+                    <A href="https://www.tu.berlin/" clazz="funder">
+                      <Img
+                        src={
+                          <ResolveAsset asset={["graphics", "tu_berlin.svg"]} />
+                        }
+                        alt="The logo of the TU Berlin."
+                      />
+                    </A>
+                  </Marginale>
+                  <P>
+                    <R n="aljoscha" /> has been employed at{" "}
+                    <AE href="https://www.tu.berlin/">TU Berlin</AE>{" "}
+                    while working on Willow.
+                  </P>
 
-                <Div clazz="clearRight" />
+                  <Div clazz="clearRight" />
 
-                <Marginale inlineable>
-                  <A href="https://iroh.computer" clazz="sponsor">
-                    <Img
-                      src={<ResolveAsset asset={["graphics", "iroh.svg"]} />}
-                      alt="The logo of Iroh."
-                    />
-                  </A>
-                </Marginale>
-                <P>
-                  We also thank our other sponsors for their support.
-                </P>
-              </Div>
-            </Footer>
-          </Div>
-        </Html5>
+                  <Marginale inlineable>
+                    <A href="https://iroh.computer" clazz="sponsor">
+                      <Img
+                        src={<ResolveAsset asset={["graphics", "iroh.svg"]} />}
+                        alt="The logo of Iroh."
+                      />
+                    </A>
+                  </Marginale>
+                  <P>
+                    We also thank our other sponsors for their support.
+                  </P>
+                </Div>
+              </Footer>
+            </Div>
+          </Html5>
+        </map>
       </BibScope>
     </Config>
   );
