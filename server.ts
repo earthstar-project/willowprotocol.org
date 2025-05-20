9768002611439f774a9d81f7eace7b0d71bcf788
Sam Gwilym <sam@gwil.garden>
2025-05-20 11:23:36 +0100

    Outline what the server needs to do and what I need to do it

--- server.ts
+++ server.ts
@@ -1,13 +1,28 @@
 import { serveDir } from "https://deno.land/std@0.207.0/http/file_server.ts";
 
-// TODO: Random emblem serving.
-// TODO: Request for hashed content forever caching.
-// TODO: Request for etagged content caching.
-// TODO: Request for regular content caching.
-// TODO: Add a server arg for this script which turns off caching.
-// TODO: 404 response.
-
 Deno.serve((req: Request) => {
+  // Normalise the path.
+  // If path has a trailing slash, remove it.
+  // Formulate the corresponding file path.
+  //
+  // Is the path for a willow emblem?
+  //  then serve up a random one.
+  //
+  // Are we in development mode? Just serve the response with no caching headers.
+  //
+  // TODO (aljoscha): Could we have a file containing JSON with an array of all content-addressed paths?
+  // Is the path a content-addressed asset?
+  //  then serve up a response with a very very long cache expiry date.
+  //
+  // TODO (aljoscha): Could we have a file containing JSON with a mapping of build paths (e.g. /specs/index.html) to their corresponding etags?
+  // Does the request have an etag, and the asset have an etag?
+  //  then send 304 if etag is equivalent
+  //  or send response with content otherwise (with etag caching header)
+  //
+  // Do we even have this asset?
+  //  if yes, serve with common sense caching headers
+  //  if no, return a 404 response and page.
+
   return serveDir(req, {
     fsRoot: "build",
     enableCors: true, // Required so that preview iframes can access fonts (for katex).
