063995dba3e05893f6271c0acab07f947c9c358d
Sam Gwilym <sam@gwil.garden>
2025-05-21 13:31:29 +0100

    Implement new fun server!

--- server.ts
+++ server.ts
@@ -1,30 +1,140 @@
-import { serveDir } from "https://deno.land/std@0.207.0/http/file_server.ts";
+import cachingInfo from "./build/cachinginfo.json" with { type: "json" };
+import { extname, join } from "@std/path";
+import { contentType } from "@std/media-types";
+import { parseArgs } from "jsr:@std/cli/parse-args";
 
-Deno.serve((req: Request) => {
+const emblemFileNames = [
+  "a.png",
+  "b.png",
+  "c.png",
+];
+
+async function pickRandomEmblem() {
+  const filename =
+    emblemFileNames[Math.floor(Math.random() * emblemFileNames.length)];
+
+  const file = await Deno.open(
+    join("src", "assets", "emblems", filename),
+  );
+
+  return file.readable;
+}
+
+Deno.serve(async (req: Request) => {
+  const flags = parseArgs(Deno.args, {
+    boolean: ["dev"],
+  });
+
+  const isDevMode = flags.dev;
+
   // Normalise the path.
-  // If path has a trailing slash, remove it.
+
+  const url = new URL(req.url);
+  let normalisedPath = url.pathname;
+  const extension = extname(url.pathname);
+
+  // If path has a trailing slash, add index.html
+  if (normalisedPath.endsWith("/")) {
+    normalisedPath = `${normalisedPath}index.html`;
+  }
+
   // Formulate the corresponding file path.
-  //
+  const fsPath = join(
+    ".",
+    "build",
+    normalisedPath,
+  );
+
+  // Let's get a handle on that path.
+  const file = await Deno.open(fsPath).catch(() => undefined);
+
+  // If there's no file there, it's time to 404.
+  if (!file) {
+    console.warn(404, "NOT FOUND", normalisedPath);
+
+    return new Response("No page found", {
+      status: 404,
+    });
+  }
+
   // Is the path for a willow emblem?
   //  then serve up a random one.
-  //
+  if (url.pathname === "/emblem.png") {
+    console.log(200, url);
+
+    return new Response(await pickRandomEmblem(), {
+      headers: {
+        "Content-Type": "image/png",
+        "Cache-Control": "s-max-age=3600,max-age=3600,public",
+      },
+    });
+  }
+
+  const contentKind = extension === ""
+    ? "text/html"
+    : contentType(extension) || "text/plain";
+
   // Are we in development mode? Just serve the response with no caching headers.
-  //
-  // TODO (aljoscha): Could we have a file containing JSON with an array of all content-addressed paths?
+  if (isDevMode) {
+    console.log(200, "dev", url.pathname);
+
+    return new Response(file.readable, {
+      headers: {
+        "Content-Type": contentKind,
+      },
+    });
+  }
+
   // Is the path a content-addressed asset?
   //  then serve up a response with a very very long cache expiry date.
-  //
-  // TODO (aljoscha): Could we have a file containing JSON with a mapping of build paths (e.g. /specs/index.html) to their corresponding etags?
-  // Does the request have an etag, and the asset have an etag?
-  //  then send 304 if etag is equivalent
-  //  or send response with content otherwise (with etag caching header)
-  //
-  // Do we even have this asset?
-  //  if yes, serve with common sense caching headers
-  //  if no, return a 404 response and page.
+  if (cachingInfo.contentAddressedFiles.includes(normalisedPath)) {
+    console.log(200, "content-addressed", url.pathname);
 
-  return serveDir(req, {
-    fsRoot: "build",
-    enableCors: true, // Required so that preview iframes can access fonts (for katex).
+    return new Response(file.readable, {
+      headers: {
+        "Content-Type": contentKind,
+        "Cache-Control": "s-max-age=31536000,max-age=31536000,public",
+      },
+    });
+  }
+
+  // Does the request have an etag, and the asset have an etag?
+  const etag = (cachingInfo.etags as Record<string, string>)[normalisedPath];
+  if (etag) {
+    const ifNoneMatch = req.headers.get("If-None-Match");
+
+    // If the request has an equivalent etag, send status 304
+    if (
+      ifNoneMatch && (ifNoneMatch === `W/${etag}` || ifNoneMatch === etag)
+    ) {
+      console.log(304, url.pathname);
+
+      return new Response(undefined, {
+        status: 304,
+      });
+    }
+
+    // Or send the content with the current etag.
+    console.log(200, url.pathname);
+
+    return new Response(file.readable, {
+      headers: {
+        "Content-Type": contentKind,
+        "ETag": etag,
+        "Cache-Control": "s-max-age=3600,max-age=3600,public",
+      },
+    });
+  }
+
+  // (Covering our butts)
+  // There's no etag, but there is a file.
+  // Just send with common sense caching policy.
+  console.warn(200, "NOT ETAGGED!", url.pathname);
+
+  return new Response(file.readable, {
+    headers: {
+      "Content-Type": contentKind,
+      "Cache-Control": "s-max-age=3600,max-age=3600,public",
+    },
   });
 });
