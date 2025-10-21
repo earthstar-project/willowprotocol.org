cffdfa46be1f8f3c26d741db64abc44a39473bfb
Aljoscha Meyer <mail@aljoscha-meyer.de>
2025-10-21 17:10:15 +0200

    Write overly long and sappy acknowledgements

--- src/pages/more/about_us.tsx
+++ src/pages/more/about_us.tsx
@@ -94,8 +94,42 @@ export const about_us = (
 
         <Hsection title="Acknowledgements" n="special_thanks">
           <P>
-            Our special thanks to Ar Nazeh, Marce Coll, Joakim Stai, and Cade
-            Diehm.
+            Once upon a time, Willow had no notion of subspaces, instead there
+            were only the public keys of authors. There were no generalised
+            authorisation tokens either, only signatures. And then{" "}
+            <AE href="https://nuh.dev/">Nuh</AE>{" "}
+            pointed out to us that we could plug in a capability system there.
+            Thank you for that!
+          </P>
+
+          <P>
+            <AE href="https://dziban.net/">Marcelino Coll</AE>{" "}
+            pointed out an error in an earlier draft of the{" "}
+            <R n="willow_confidential_sync">Confidential Sync</R>{" "}
+            specification. In the process of fixing the confidentiality leak, we
+            also figured out how to make the protocol resistant against active
+            eavesdroppers. Without his careful reading, that might never have
+            happened!
+          </P>
+
+          <P>
+            One calm day not too long ago,{" "}
+            <AE href="https://joakim.io/">Joakim</AE>{" "}
+            sent us an email with ample of information on how UNIX handles leap
+            seconds. Spoilers: it is bad. He also pointed us to{" "}
+            <AE href="https://en.wikipedia.org/wiki/International_Atomic_Time">
+              International Atomic Time
+            </AE>{" "}
+            as the better alternative. This is now how we recommend setting
+            timestamps! Thank you for not letting us get away with duplicated
+            leap seconds that would have potentially caused the most confounding
+            bugs once every couple of years.
+          </P>
+
+          <P>
+            <AE href="https://shiba.computer/">Cade</AE>{" "}
+            has been an inspiration and aspiration for Willow throughout its
+            creation. Thank you.
           </P>
         </Hsection>
       </PageTemplate>
